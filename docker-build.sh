508f4c6f928aedae092cc716e657d3963e25669c
Robert Zhou <alphacodinghub@outlook.com>
2020-05-25 00:06:21 +0100

    initial

---
+++ docker-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build -f Dockerfile-7-3.docker -t alphacodinghub/php-fpm:7.3-alpine .
+docker build -f Dockerfile-7-3.docker -t alphacodinghub/php-fpm:latest .
+
+docker build -f Dockerfile-5-6.docker -t alphacodinghub/php-fpm:5.6-alpine .
